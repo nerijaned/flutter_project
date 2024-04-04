de46300d4acfb02abf63dd059a56b98b2a2d170f
nerijaned <nerizzajane.dejesus@gmail.com>
2024-04-04 15:39:57 -0600

    Commit 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/screens/my_home_page.dart';
+import 'package:flutter_project/screens/settings.dart';
+import 'package:flutter_project/screens/profile.dart';
 
 void main() {
   runApp(const MainApp());
@@ -21,6 +23,10 @@ class _MainAppState extends State<MainApp> {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
-    );
+      initialRoute: '/',
+      routes: {
+        '/settings' : (context) => Settings(),
+        '/profile' : (context) => Profile(),
+      });
   }
 }
--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -9,18 +9,59 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  List<Widget> imageList = [
-    Image.asset('assets/images/images1.jpg'),
-    Image.asset('assets/images/images2.jpg'),
-    Image.asset('assets/images/images3.jpg'),
-    Image.asset('assets/images/images4.jpg'),
-    Image.asset('assets/images/images5.jpg'),
-    Image.asset('assets/images/images6.jpg')
-  ];
-
-
   @override
   Widget build(BuildContext context) {
+
+    List<String> imageList = [
+      'assets/images/images1.jpg',
+      'assets/images/images2.jpg',
+      'assets/images/images3.jpg',
+      'assets/images/images4.jpg',
+      'assets/images/images5.jpg',
+      'assets/images/images6.jpg'
+    ];
+
+    List <String> imageTitle = [
+      'Lavenders',
+      'Daisies',
+      'Roses',
+      'Sunflowers',
+      'Tulips',
+      'Lilies'
+    ];
+
+    double Width = MediaQuery.of(context).size.width;
+    double Height = MediaQuery.of(context).size.height;
+
+    List<Widget> gridStuff = List.generate(6,(index) {
+      return Container(
+        width: Width,
+        height: Height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          image: DecorationImage(
+            image: AssetImage(imageList[index]),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Text(imageTitle[index], style: TextStyle(
+            color: Colors.white,
+            fontSize: 35,
+            fontWeight: FontWeight.bold,
+            shadows: [
+              Shadow(
+                blurRadius: 5,
+                color: Colors.black.withOpacity(0.5),
+                offset: Offset(5, 5),
+              )
+            ]
+          ),),
+        ),
+      );
+    });
+
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 255, 163, 213),
@@ -43,16 +84,30 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       drawer: const Drawer(),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children:List.generate(6, (index) => 
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            imageList[index],
-          ],
-        ))),
-      bottomNavigationBar: BottomNavigationBar(items: const [
+      body: 
+      Container(
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 255, 163, 213),
+        ),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, 
+            mainAxisExtent: 240,
+            ),
+            itemCount: gridStuff.length,
+            itemBuilder: (BuildContext context, int index){
+              return Column(
+                children: [
+                  Container(height: 240, child: gridStuff[index]),
+                ],
+              );
+            }),
+      ),
+
+
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: true,
+        items: const [
         BottomNavigationBarItem(
             backgroundColor: Color.fromARGB(255, 255, 163, 213),
             icon: Icon(Icons.home),
